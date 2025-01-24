787194eb1b84c89dc0fc409d88b7dc9d62958ac9
deanrobin333 <deanrobin777@gmail.com>
2025-01-24 05:07:46 +0000

    successfully setup reverse proxy using nginx for flask app - http://shopright.dynofin.com/ opens succesfully without specifying port number

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='164.92.96.222', port=5000, debug=True)
+    app.run(host='164.92.96.222', port=3333, debug=True)
